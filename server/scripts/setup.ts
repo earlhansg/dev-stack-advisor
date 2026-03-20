fe40876ff72127e8559c6db710ceb65a671b70e2
earlhansg <earlhansg@gmail.com>
2026-03-20 21:08:22 +0800

    added script for programmatically setting up vectore store

---
+++ server/scripts/setup.ts
@@ -0,0 +1,55 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import * as fs from "fs";
+import * as path from "path";
+import OpenAI from "openai";
+
+// Initialize OpenAI client
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Map folders to their respective files
+const STORE_CONFIG = [
+  {
+    name: "vs_technology_profiles",
+    folder: "./data/store-1-technology-profiles",
+  },
+];
+
+async function createOrGetVectorStore(name: string) {
+  const vs = await openai.vectorStores.create({
+    name,
+  });
+
+  console.log(`Vector store '${name}' is ready. (ID: ${vs.id})`);
+  return vs.id;
+}
+
+async function uploadFolderToStore(vectorStoreId: string, folderPath: string) {
+  const files = fs
+    .readdirSync(folderPath)
+    .filter((file: string) => file.endsWith(".txt"))
+    .map((file: string) => fs.createReadStream(path.join(folderPath, file)));
+  if (files.length === 0) {
+    console.warn(`No .text files found in folder: ${folderPath}`);
+    return;
+  }
+  await openai.vectorStores.fileBatches.uploadAndPoll(vectorStoreId, { files });
+
+  console.log(`Uploaded files from ${folderPath}`);
+}
+
+async function setup() {
+    for (const store of STORE_CONFIG) {
+        const vectorStoreId = await createOrGetVectorStore(store.name);
+        await uploadFolderToStore(vectorStoreId, store.folder);
+
+        console.log("------------------------------");
+    }
+    console.log("All vector stores are ready!");
+}
+
+setup();
